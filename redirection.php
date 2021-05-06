d726db806059b4e1e15530fcb71a7a2e0d99a703
maxime-fleury <m.fleury@codeur.online>
2021-05-06 16:40:25 +0200

    fix some redirection bugs

--- redirection.php
+++ redirection.php
@@ -2,11 +2,14 @@
 //Ce fichier permet d'afficher un message "redirection en cours" de manière plus propre qu'une simple redirection
 //C'est à dire que au lieu d'attendre x secondes comme nous ne somme pas connecté pour aller sur index.php
 //Cela pourrai aussi permettre pourquoi pas de logger les abus etc ...
-//Car on peut pas écrire du texte avant une redirection instanté via php
+//On ne peut pas écrire du texte avant une redirection instanté via php
 $err = "";
 if(isset($_GET["err"])){
     $err = $_GET["err"];
     echo "Redirection en cours ! Car " . $err; 
+    echo "<meta http-equiv='refresh' content='1;URL=index.php?err=" . $err . "'>";
 }
-
-echo '<meta http-equiv="refresh" content="1;URL=index.php?err=' . $err . '">';
+else{
+ $err = "ERROR !!!";
+ echo "<meta http-equiv='refresh' content='1;URL=index.php?err=" . $err . "'>";
+}
